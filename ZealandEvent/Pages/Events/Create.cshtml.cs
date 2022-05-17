b4bc0fe003a174992fe5aafe4e2034f7241426a2
Cem Turan <cemx0121@edu.zealand.dk>
2022-05-17 10:02:51 +0200

    ny kode + gui

--- ZealandEvent/Pages/Events/Create.cshtml.cs
+++ ZealandEvent/Pages/Events/Create.cshtml.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using ZealandEventLib.Data;
 using ZealandEventLib.Models;
 
@@ -21,25 +22,49 @@ namespace ZealandEvent.Pages.Events
             _context = context;
         }
 
-        public IActionResult OnGet()
+        public async Task<IActionResult> OnGet(int? id)
         {
-        ViewData["ArrangementId"] = new SelectList(_context.Arrangements, "ArrangementId", "Name");
+            Arrangement = await _context.Arrangements.FirstOrDefaultAsync(m => m.ArrangementId == id);
             return Page();
         }
 
         [BindProperty]
         public Event Event { get; set; }
+        public Event EventAlreadyExist { get; set; }
+        public Arrangement Arrangement { get; set; }
+        public string Message { get; set; }
 
         // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
-        public async Task<IActionResult> OnPostAsync()
+        public async Task<IActionResult> OnPostAsync(int? id)
         {
+            
             if (!ModelState.IsValid)
             {
                 return Page();
             }
 
-            _context.Events.Add(Event);
-            await _context.SaveChangesAsync();
+            Arrangement = await _context.Arrangements.FirstOrDefaultAsync(m => m.ArrangementId == id);
+            Event.ArrangementId = Arrangement.ArrangementId;
+            
+            // IKKE FÆRDIGT
+            EventAlreadyExist = await _context.Events.Where(e => 
+            e.ArrangementId == Event.ArrangementId && 
+            ((e.Start <= Event.Start && e.End >= Event.Start) || (e.End >= Event.End && e.Start <= Event.End))
+
+            ).FirstOrDefaultAsync();
+         
+
+            if (EventAlreadyExist != null)
+            {
+                Message = "Der findes allerede et event i samme tidsrum & lokation til dette arrangement!";
+                return Page();
+            }
+            else
+            {
+
+                _context.Events.Add(Event);
+                await _context.SaveChangesAsync();
+            }
 
             return RedirectToPage("/Arrangementer/Index");
         }
